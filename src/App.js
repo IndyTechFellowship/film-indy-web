6efebc87089d15affc1a7c10fa63702d36f438d0
ejmudrak <ejmudrak@gmail.com>
2017-09-10 15:39:33 -0400

    Adding Film Indy colors and links to tech docs

--- src/App.js
+++ src/App.js
@@ -8,11 +8,18 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Film Indy Project</h2>
+        </div>
+        <div className="App-subheader">
+            <h3>Built with <a href="https://github.com/facebookincubator/create-react-app" className="App-docs">React,</a>
+                <a href="http://redux.js.org/" className="App-docs">Redux,</a>
+                <a href="https://firebase.google.com/docs/" className="App-docs">& Firebase</a>
+            </h3>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
       </div>
     );
   }
