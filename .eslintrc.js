124d09f8866f3a4b847e36979e4da6e87fe7a362
Kyle Potts <kyle.potts1@angieslist.com>
2017-09-15 15:15:13 -0400

    add some overidden eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,9 @@ module.exports = {
         "browser": true
     },
     "rules": {
+        "max-len": ['error', 500],
         "semi": ["error", "never"],
+        'react/prefer-stateless-function': ['warn', { ignorePureComponents: true }],
         "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
         "import/prefer-default-export": 'off',
         'import/no-named-as-default-member': 'off',
