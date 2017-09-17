758a48090e90bdf2eabc27fb8c1c98c13d9d85e1
Kyle Potts <kyle@kylepotts.com>
2017-09-17 18:08:48 -0400

    add static.json

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './App.css'
 
 import AppBar from 'material-ui/AppBar'
 import Avatar from 'material-ui/Avatar'
-import {Card} from 'material-ui/Card'
+import { Card } from 'material-ui/Card'
 import TextField from 'material-ui/TextField'
 
 import SearchIcon from 'material-ui/svg-icons/action/search'
@@ -17,26 +17,26 @@ const Logo = require('./film-indy-logo.png')
 
 const App = () => (
   <div className="App">
-      <AppBar
-          iconElementLeft={
-              <div>
-                  <img src={Logo} className="logo" alt="Film Indy Logo"/>
-                  <Card className="searchCard" style={{width: 400}}>
-                          <SearchIcon className="searchIcon" />
-                          <TextField
-                              className="searchField"
-                              hintText="Search FilmIndy"
-                              underlineFocusStyle={{borderColor: '#38b5e6'}}
-                              floatingLabelFocusStyle={{color: '#38b5e6'}}
-                          />
-                  </Card>
-              </div>
-          }
-          iconElementRight={ <Avatar className="accountIcon" src="https://goo.gl/ybdoo6" size={60} /> }
-          zDepth = {2}
-      />
-     <Route exact path="/" component={Home} />
-     <Route exact path="/dashboard" component={Dashboard} />
+    <AppBar
+      iconElementLeft={
+        <div>
+          <img src={Logo} className="logo" alt="Film Indy Logo" />
+          <Card className="searchCard" style={{ width: 400 }}>
+            <SearchIcon className="searchIcon" />
+            <TextField
+              className="searchField"
+              hintText="Search FilmIndy"
+              underlineFocusStyle={{ borderColor: '#38b5e6' }}
+              floatingLabelFocusStyle={{ color: '#38b5e6' }}
+            />
+          </Card>
+        </div>
+      }
+      iconElementRight={<Avatar className="accountIcon" src="https://goo.gl/ybdoo6" size={60} />}
+      zDepth={2}
+    />
+    <Route exact path="/" component={Home} />
+    <Route exact path="/dashboard" component={Dashboard} />
     <Route exact path="/login" component={Login} />
   </div>
 )
