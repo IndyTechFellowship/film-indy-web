ff29bc7ffb39ae82dc94dd65ecfd313b543b92db
Kyle Potts <pottsk@purdue.edu>
2017-09-29 07:57:38 -0400

    migrate user on signUp

--- src/redux/actions/creators/accountActions.js
+++ src/redux/actions/creators/accountActions.js
@@ -1,19 +1,41 @@
 import * as firebase from 'firebase'
 import { push } from 'react-router-redux'
-import { SIGN_IN } from '../types/accountActionTypes'
-import { SIGN_UP } from '../types/accountActionTypes'
+import { SIGN_IN, SIGN_UP } from '../types/accountActionTypes'
 
 /* this an example of how to chain actions together.
 This is a function which takes username and email and and returns a function with the argument of dispatch
 which can be used inside the function to dispatch events.
 In this case we dispatch the signIn actions and then then it is finished we dispatch a push to the router to go the /dashboard */
 
+const migrateIfNeeded = (email) => {
+  const userMigrationRef = firebase.database().ref('/userMigration')
+  userMigrationRef.orderByChild('email').equalTo(email).once('value').then((snapshot) => {
+    const dataToMigrate = snapshot.val()
+    if (dataToMigrate !== null) {
+      const uid = firebase.auth().currentUser.uid
+      const key = Object.keys(dataToMigrate)[0]
+      const userToMigrate = dataToMigrate[key]
+      const roles = userToMigrate.roles
+      const firstName = userToMigrate.firstName
+      const lastName = userToMigrate.lastName
+      const profilesRef = firebase.database().ref(`/userProfiles/${uid}/roles`)
+      const accountRef = firebase.database().ref(`/userAccount/${uid}`)
+      profilesRef.set(roles)
+      accountRef.set({
+        firstName,
+        lastName
+      })
+    }
+  })
+}
+
 export const signIn = (email, password) => dispatch => dispatch({
   type: SIGN_IN,
-  payload: firebase.auth().signInWithEmailAndPassword(email, password),
-}).then(() => dispatch(push('dashboard')));
+  payload: firebase.auth().signInWithEmailAndPassword(email, password)
+}).then(() => dispatch(push('dashboard')))
 
 export const signUp = (email, password) => dispatch => dispatch({
   type: SIGN_UP,
-  payload: firebase.auth().createUserWithEmailAndPassword(email, password),
-}).then(() => dispatch(push('dashboard')));
+  payload: firebase.auth().createUserWithEmailAndPassword(email, password)
+}).then(() => dispatch(push('dashboard'))).then(() => migrateIfNeeded(email))
+
