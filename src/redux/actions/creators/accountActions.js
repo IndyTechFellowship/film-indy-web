cd95975726bb46a5b48a4dba12b8ef7fca4b4048
Aaron Humphrey <aarhumph@indiana.edu>
2017-10-05 23:40:47 -0400

    updates account first/last name, needs photo

--- src/redux/actions/creators/accountActions.js
+++ src/redux/actions/creators/accountActions.js
@@ -4,9 +4,37 @@ import { SIGN_IN, SIGN_UP, SIGN_OUT } from '../types/accountActionTypes'
 import * as algoliaActions from './algoliaActions'
 
 /* this an example of how to chain actions together.
-This is a function which takes username and email and and returns a function with the argument of dispatch
-which can be used inside the function to dispatch events.
-In this case we dispatch the signIn actions and then then it is finished we dispatch a push to the router to go the /dashboard */
+ This is a function which takes username and email and and returns a function with the argument of dispatch
+ which can be used inside the function to dispatch events.
+ In this case we dispatch the signIn actions and then then it is finished we dispatch a push to the router to go the /dashboard */
+
+const migrateOrUpdate = (firstName, lastName, email) => {
+  const userMigrationRef = firebase.database().ref('/userMigration')
+  userMigrationRef.orderByChild('email').equalTo(email).once('value').then((snapshot) => {
+    const dataToMigrate = snapshot.val()
+    if (dataToMigrate !== null) {
+      const uid = firebase.auth().currentUser.uid
+      const key = Object.keys(dataToMigrate)[0]
+      const userToMigrate = dataToMigrate[key]
+      const roles = userToMigrate.roles
+      const firstName = userToMigrate.firstName
+      const lastName = userToMigrate.lastName
+      const profilesRef = firebase.database().ref(`/userProfiles/${uid}/roles`)
+      const accountRef = firebase.database().ref(`/userAccount/${uid}`)
+      profilesRef.set(roles)
+      accountRef.set({
+        firstName,
+        lastName
+      })
+    }
+  })
+  const uid = firebase.auth().currentUser.uid
+  const accountRef = firebase.database().ref(`/userAccount/${uid}`)
+  accountRef.set({
+    firstName,
+    lastName
+  })
+}
 
 const encodeAsFirebaseKey = string => string.replace(/%/g, '%25')
   .replace(/\./g, '%2E')
@@ -43,7 +71,7 @@ export const signIn = (email, password) => dispatch => dispatch({
   payload: firebase.auth().signInWithEmailAndPassword(email, password)
 }).then(() => dispatch(push('account')))
 
-export const signUp = (fullName, email, password) => dispatch => dispatch({
+export const signUp = (firstName, lastName, email, password) => dispatch => dispatch({
   type: SIGN_UP,
   payload: firebase.auth().createUserWithEmailAndPassword(email, password)
 }).then(result => migrate(email, result, dispatch))
--- src/redux/actions/types/accountActionTypes.js
+++ src/redux/actions/types/accountActionTypes.js
@@ -1,3 +1,6 @@
 export const SIGN_IN = 'SIGN_IN';
 export const SIGN_UP = 'SIGN_UP';
+<<<<<<< HEAD
 export const SIGN_OUT = 'SIGN_OUT';
+=======
+>>>>>>> updates account first/last name, needs photo
