711228f7aaf523a39389b38aec50ebd8b364dd90
ejmudrak <ejmudrak@gmail.com>
2017-10-07 16:54:59 -0400

    Update profile structure in place finally

--- src/presentation/account/accountPage.js
+++ src/presentation/account/accountPage.js
@@ -1,6 +1,5 @@
 import React from 'react'
-
-
+import { Field, reduxForm } from 'redux-form'
 import PropTypes from 'prop-types'
 import { get } from 'lodash'
 
@@ -18,66 +17,112 @@ import UploadIcon from 'material-ui/svg-icons/file/file-upload'
 import '../../App.css'
 import './accountPage.css'
 
-const accountPage = (props) => {
-      const { profile, firebase, auth } = this.props
-      const photoURL = get(profile, 'photoURL', '')
-      const email = get(auth, 'email')
-      const displayName = get(profile, 'displayName')
-      const uid = get(auth, 'uid')
-      return (
-        <div>
-          <h1>Account Settings</h1>
-          <Card className="profileCard" >
-            <CardTitle title="Your Profile" />
-            <Divider />
-            <div className="imageWrapper">
-              <Avatar className="accountImage" src={photoURL} size={150} />
-              <CameraIcon className="cameraIcon" color="white" />
-              <FlatButton className="imageText" icon={<UploadIcon />} label="Upload Picture" labelPosition="before" containerElement="label">
-                <FileUploader uid={uid} uploadFile={firebase.uploadFile} updateProfile={firebase.updateProfile} />
-              </FlatButton>
-            </div>
-            <ul className="fields">
-              <li>
-                  <TextField
-                      className="profileField"
-                      hintText="Enter your name"
-                      floatingLabelText="Name"
-                      defaultValue={displayName}
-                      value={this.state.currentName}
-                      onChange={this.handleNameChange}
-                      type="text"
-                   />
-              </li>
-              <li>
-                  <TextField
-                      className="profileField"
-                      hintText="Enter your email"
-                      floatingLabelText="Email"
-                      defaultValue={email}
-                      value={this.state.currentEmail}
-                      onChange={this.handleEmailChange}
-                      type="text"
-                   />
-              </li>
-            </ul>
-            <RaisedButton className="accountButton" primary label="Save"/>
-            {/* <RaisedButton className="accountButton" primary label="Save" onClick={firebase.updateProfile({displayName: this.state.currentName, email: this.state.email })}/> */}
-          </Card>
-          <Card className="passwordCard">
-            <CardTitle title="Reset Password" />
-            <Divider />
-            <ul className="fields">
-              <li><TextField hintText="New Password" floatingLabelText="Password" type="password" /></li>
-              <li><TextField hintText="Confirm Password" floatingLabelText="Confirm Password" type="password" /></li>
-            </ul>
-            <RaisedButton className="accountButton" primary label="Submit" />
-          </Card>
+const validate = values => {
+    const errors = {};
+
+    if (!values.firstName || !values.lastName || !values.email) {
+        if (!values.firstName) {
+            errors.firstName = "You forgot to enter your first name!";
+        }
+        if (!values.lastName) {
+            errors.lastName = "You forgot to enter your last name!";
+        }
+        if (!values.email) {
+            errors.email = "You forgot to enter an email!";
+        }
+    }
+    return errors;
+};
+
+// Making component const throws an error that it is read-only
+const AccountPage = (props) => {
+
+  function handleSubmit (firstName, lastName, email) { firebase.updateProfile({firstName, lastName, email}) }
+
+  const { profile, firebase, auth } = props;
+  const photoURL = get(profile, 'photoURL', '')
+  const firstName = get(profile, 'firstName', '')
+  const lastName = get(profile, 'lastName', '')
+  const email = get(auth, 'email')
+  const uid = get(auth, 'uid')
+
+  return (
+    <div>
+      <h1>Account Settings</h1>
+      <Card className="profileCard" >
+        <CardTitle title="Your Profile" />
+        <Divider />
+        <div className="imageWrapper">
+          <Avatar className="accountImage" src={photoURL} size={150} />
+          <CameraIcon className="cameraIcon" color="white" />
+          <FlatButton className="imageText" icon={<UploadIcon />} label="Upload Picture" labelPosition="before" containerElement="label">
+            <FileUploader uid={uid} uploadFile={firebase.uploadFile} updateProfile={firebase.updateProfile} />
+          </FlatButton>
         </div>
-      )
+        <form onSubmit={this.handleSubmit}>
+            <ul className="fields">
+              <li>
+                <div>
+                    <Field
+                    name="firstName"
+                    component={TextField}
+                    floatingLabelText="First Name"
+                    type="text"
+                    />
+                </div>
+              </li>
+              <li>
+                <div>
+                    <Field
+                    name="lastName"
+                    component={TextField}
+                    floatingLabelText="Last Name"
+                    type="text"
+                    />
+                </div>
+              </li>
+              <li>
+                <div>
+                    <Field
+                    name="email"
+                    component={TextField}
+                    floatingLabelText="Email"
+                    type="email"
+                    />
+                </div>
+              </li>
+            </ul>
+          <RaisedButton className="accountButton" primary label="Save" />
+        </form>
+      </Card>
+
+      <Card className="passwordCard">
+        <CardTitle title="Reset Password" />
+        <Divider />
+        <ul className="fields">
+          <li><TextField hintText="New Password" floatingLabelText="Password" type="password" /></li>
+          <li><TextField hintText="Confirm Password" floatingLabelText="Confirm Password" type="password" /></li>
+        </ul>
+        <RaisedButton className="accountButton" primary label="Submit" />
+      </Card>
+    </div>
+  )
 }
 
-const FileUploader = props => (
+// // Decorate with reduxForm(). It will read the initialValues prop provided by connect()
+// AccountPage = reduxForm({
+//   form: 'accountPage', // a unique identifier for this form
+// })(AccountPage);
+
+// // You have to connect() to any reducers that you wish to connect to yourself
+// AccountPage = connect(
+//   state => ({
+//     initialValues: state.account.data, // pull initial values from account reducer
+//   }),
+//   { load: loadAccount }, // bind account loading action creator
+// )(AccountPage);
+
+const FileUploader = props => {
   <input
     name="myFile"
     type="file"
@@ -94,7 +139,7 @@ const FileUploader = props => (
       })
     }}
   />
-)
+}
 
 FileUploader.propTypes = {
   uid: PropTypes.string,
@@ -106,11 +151,11 @@ FileUploader.defaultProps = {
   uid: '',
 }
 
-accountPage.propTypes = {
+AccountPage.propTypes = {
   profile: PropTypes.shape({
-    photoURL: PropTypes.string,
     firstName: PropTypes.string,
     lastName: PropTypes.string,
+    photoURL: PropTypes.string,
   }).isRequired,
   auth: PropTypes.shape({
     uid: PropTypes.string,
@@ -122,7 +167,9 @@ accountPage.propTypes = {
   }).isRequired,
 }
 
-accountPage.defaultProps = {
-}
+const AccountPageFormEnriched = reduxForm({
+  form: 'updateProfile',
+    validate
+})(AccountPage);
 
-export default accountPage
+export default AccountPageFormEnriched
