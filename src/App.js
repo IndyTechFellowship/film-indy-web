80a5c16053408c9c570762be4fa0b5c56ea744e5
ejmudrak <ejmudrak@gmail.com>
2017-10-21 19:41:03 -0400

    Search bar renders conditionally

--- src/App.js
+++ src/App.js
@@ -4,17 +4,23 @@ import { connect } from 'react-redux'
 import { firebaseConnect } from 'react-redux-firebase'
 import { get } from 'lodash'
 import PropTypes from 'prop-types'
+import { InstantSearch, Configure } from 'react-instantsearch/dom'
+import { connectAutoComplete } from 'react-instantsearch/connectors'
+import 'react-instantsearch-theme-algolia/style.css'
 
 // Material UI Components
 import AppBar from 'material-ui/AppBar'
 import Avatar from 'material-ui/Avatar'
+import { Card } from 'material-ui/Card'
 import Menu from 'material-ui/Menu'
 import MenuItem from 'material-ui/MenuItem'
 import Popover from 'material-ui/Popover'
 import Snackbar from 'material-ui/Snackbar'
+import AutoComplete from 'material-ui/AutoComplete'
 import FlatButton from 'material-ui/FlatButton'
 
 // Material UI SVG Icons
+import SearchIcon from 'material-ui/svg-icons/action/search'
 import AccountCircle from 'material-ui/svg-icons/action/account-circle'
 import LogoutIcon from 'material-ui/svg-icons/action/exit-to-app'
 import CreateIcon from 'material-ui/svg-icons/social/person-add'
@@ -35,6 +41,22 @@ import './App.css'
 import Logo from './film-indy-logo.png'
 
 import * as accountActions from './redux/actions/creators/accountActions'
+
+const ALGOLIA_SEARCH_KEY = process.env.REACT_APP_ALGOLIA_SEARCH_KEY
+const ALGOLIA_APP_ID = process.env.REACT_APP_ALGOLIA_APP_ID
+
+
+const AutoCompleteBar = connectAutoComplete(
+  ({ hits, onItemSelected }) => (
+    <AutoComplete
+      className="searchField"
+      id="autocomplete"
+      filter={AutoComplete.fuzzyFilter}
+      onNewRequest={onItemSelected}
+      dataSource={hits.map(hit => hit.roleName)}
+    />
+  )
+)
 
 class App extends React.Component {
   constructor(props) {
@@ -71,7 +93,7 @@ class App extends React.Component {
   }
 
   render() {
-    const { profile, auth, firebase } = this.props
+    const { profile, auth, firebase, history, location } = this.props
     const photoURL = get(profile, 'photoURL', '')
     const uid = get(auth, 'uid')
     return (
@@ -80,14 +102,27 @@ class App extends React.Component {
           iconElementLeft={
             <div style={{ display: 'flex', flexDirection: 'row' }}>
               <Link to="/"><img src={Logo} className="logo" alt="Film Indy Logo" /></Link>
+              { location.pathname !== '/' && // do not render search in app bar on home page
+                <Card className="menuSearchCard" style={{ width: 400 }}>
+                  <SearchIcon className="searchIcon" />
+                  <InstantSearch
+                    appId={ALGOLIA_APP_ID}
+                    apiKey={ALGOLIA_SEARCH_KEY}
+                    indexName="roles"
+                  >
+                    <Configure hitsPerPage={100} />
+                    <AutoCompleteBar onItemSelected={item => history.push({ pathname: '/search', search: `?query=${encodeURIComponent(item)}` })} />
+                  </InstantSearch>
+                </Card>
+              }
             </div>
           }
           iconElementRight={uid ? (
             <Avatar className="accountIcon" src={photoURL} size={60} onClick={this.handleTouchTap} />
           ) : (
-            <div className="buttons">
-              <Link to="/signup"><FlatButton labelStyle={{fontSize: '17px'}} style={{ color: 'white' }} label="Sign Up" size={60} /> </Link>
-              <Link to="/login"><FlatButton labelStyle={{fontSize: '17px'}} style={{ color: 'white' }} label="Login" size={60} /> </Link>
+            <div>
+              <Link to="/signup"><FlatButton style={{ color: 'white' }} label="Sign Up" size={60} /> </Link>
+              <Link to="/login"><FlatButton style={{ color: 'white' }} label="Login" size={60} /> </Link>
             </div>
           )}
           zDepth={2}
@@ -142,7 +177,8 @@ App.propTypes = {
   signOut: PropTypes.func.isRequired,
   history: PropTypes.shape({
     push: PropTypes.func
-  }).isRequired
+  }).isRequired,
+  location: PropTypes.object.isRequired
 }
 
 const wrappedApp = firebaseConnect()(App)
