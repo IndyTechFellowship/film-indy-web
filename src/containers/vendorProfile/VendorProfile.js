a01e0a768b64b496a3f3c948caa5af4c46d0cd8f
Kyle Potts <pottsk@purdue.edu>
2017-11-18 20:23:11 -0500

    add basic profile page

---
+++ src/containers/vendorProfile/VendorProfile.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { get } from 'lodash'
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
+import { firebaseConnect } from 'react-redux-firebase'
+import VendorProfile from '../../presentation/vendorProfile/vendorProfile'
+
+class EditProfileContainer extends React.Component {
+  render() {
+    return (
+      <VendorProfile {...this.props} />
+    )
+  }
+}
+
+EditProfileContainer.propTypes = {
+
+}
+
+const WrappedEditProfile = firebaseConnect((props, firebase) => {
+  const vendorId = get(props, 'match.params.vendorId', '')
+  return [
+    `/vendorProfiles/${vendorId}`
+  ]
+})(EditProfileContainer)
+
+export default withRouter(connect(
+  state => ({
+    firebase: state.firebase,
+    auth: state.firebase.auth,
+    profile: state.firebase.profile,
+    data: state.firebase.data
+
+  }),
+  { },
+)(WrappedEditProfile))
+
---
+++ src/presentation/vendorProfile/vendorProfile.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { get } from 'lodash'
+
+const ForgotPasswordPage = (props) => {
+  const vendorId = get(props, 'match.params.vendorId', '')
+  const vendorProfile = get(props, `data.vendorProfiles.${vendorId}`)
+  return (
+    <div>
+      <h3>{get(vendorProfile, 'name', '')} </h3>
+    </div>
+  )
+}
+
+ForgotPasswordPage.propTypes = {
+}
+
+ForgotPasswordPage.defaultProps = {
+}
+
+export default ForgotPasswordPage
